c06150c76d5420eecb9bf44b8e5a3325162f4c8f
Antti Alakoski <antti.alakoski@gmail.com>
2022-04-05 17:43:06 +0300

    move interface to separate file for useBooks

---
+++ client/src/interfaces/book.ts
@@ -0,0 +1,29 @@
+interface ISBN {
+  isbn10: number;
+  isbn13: string;
+}
+
+export interface Book {
+  rank: number;
+  rank_last_week: number;
+  weeks_on_list: number;
+  asterisk: number;
+  dagger: number;
+  primary_isbn10: number;
+  primary_isbn13: string;
+  publisher: string;
+  description: string;
+  price: number;
+  title: string;
+  author: string;
+  contributor: string;
+  contributor_note: string;
+  book_image: string;
+  amazon_product_url: string;
+  age_group: string;
+  book_review_link: string;
+  first_chapter_link: string;
+  sunday_review_link: string;
+  article_chapter_link: string;
+  isbns: ISBN[];
+}
