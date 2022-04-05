d0aba66c703641569e0bd48044012f2de5b87f73
Antti Alakoski <antti.alakoski@gmail.com>
2022-04-05 17:39:02 +0300

    refactor reviews component a bit to clean some ui clutter

--- client/src/components/reviews/Reviews.tsx
+++ client/src/components/reviews/Reviews.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import classes from './Reviews.module.css';
 import useReviews from '../../hooks/useReviews';
 import Loading from '../loading/Loading';
+import Review from './Review';
 const Reviews = ({ isbn }: { isbn: number }) => {
   const [show, setShow] = useState(false);
   const { reviews, loading, fetchReviews, fetched } = useReviews(isbn);
@@ -41,26 +42,7 @@ const Reviews = ({ isbn }: { isbn: number }) => {
         ) : fetched && reviews.length ? (
           <ul className={classes['review-list']}>
             {reviews.map(({ url, byline }) => (
-              <li key={url}>
-                <a href={url} rel="noreferrer" target="_blank">
-                  {byline}
-                </a>
-                <span className={classes.external}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                    />
-                  </svg>
-                </span>
-              </li>
+              <Review key={url} author={byline} url={url} />
             ))}
           </ul>
         ) : (
